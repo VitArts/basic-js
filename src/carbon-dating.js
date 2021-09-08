ac5b00eaa1514149b80bf5760cfd20cd25ad3c4c
VitArts <vitarts@ya.ru>
2021-09-08 16:34:30 +0300

    init: basic-js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,7 +19,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string') { isNaN(parseFloat(sampleActivity) || (!isNaN(sampleActivity) && (+sampleActivity <= 0 || +sampleActivity > 15)))
+     {
+      return false
+    }
+  }
+  let res = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (LN2 / HALF_LIFE_PERIOD))
+  return res
+  }
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let name = ""
+
+  if (Array.isArray(members)) {
+    members = members.map(item => item.trim())
+    members.sort()
+   for (let i = 0; i < members.length; i++) {
+     if (typeof(members[i]) == 'string') {
+        name += members[i][0]
+     }
+   }
+   return name.toUpperCase()
+  }
+
+  return false
 }
