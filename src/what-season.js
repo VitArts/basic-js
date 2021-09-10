904975a88d973653bd41242ad3099e207d1ed737
VitArts <vitarts@ya.ru>
2021-09-10 16:40:30 +0300

    add: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,32 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  getSeason(new Date(1375, 5, 1, 12, 18, 41, 652))
+//  getSeason(new Date(1994, 1, 2, 3, 4, 5))
+//  getSeason(new Date(1, 4, 6, 7, 8, 9))
+
+
+export default function getSeason(date) {
+
+  const arr = String(date).split(' ');
+
+if (arr[1] == 'Feb' || arr[1] == 'Jan' || arr[1] == 'Dec') {
+  return 'winter'
+}
+
+if (arr[1] == 'Mar' || arr[1] == 'Apr' || arr[1] == 'May') {
+  return 'spring'
+}
+
+if (arr[1] == 'Jun' || arr[1] == 'Jul' || arr[1] == 'Aug') {
+  return 'summer'
+}
+
+if (arr[1] == 'Sep' || arr[1] == 'Oct' || arr[1] == 'Nov') {
+  return 'autumn'
+}
+
+//consolereturn 'Invalid date!'
+
+
 }
