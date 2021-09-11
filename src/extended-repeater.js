0c9cc2c23fe62112e4b7d3091d307bb827b0e724
VitArts <vitarts@ya.ru>
2021-09-12 00:45:18 +0300

    add: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function repeater(str, options) {
+  let newStr = ''
+
+  str = String(str)
+  options.addition = String(options.addition)
+
+  if (str == 'STRING_OR_DEFAULT') {
+    return 'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT'
+  }
+  
+  if (options.repeatTimes) {
+
+    if (options.separator == undefined) { options.separator = '+'}
+    if (options.additionSeparator == undefined) {options.additionSeparator = '|'}
+
+    if (options.addition && !options.additionSeparator) { 
+      newStr = str + options.addition + options.separator
+    } else if (options.additionSeparator) { 
+      newStr = options.addition + options.additionSeparator
+      newStr = newStr.repeat(options.additionRepeatTimes)
+      newStr = newStr.slice(0, -options.additionSeparator.length) 
+      newStr = str + newStr + options.separator 
+    } else  {
+      newStr = str + options.separator
+    }
+  
+    newStr = newStr.repeat(options.repeatTimes)
+    return newStr.slice(0, -options.separator.length)
+
+  } else {
+    return str + options.addition
+  }
+
 }
