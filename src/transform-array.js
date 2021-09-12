d10b6579286bb89970c2c687ed447e22f7493ea3
VitArts <vitarts@ya.ru>
2021-09-12 16:07:04 +0300

    add: transfrom-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,75 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  transform([1, 2, 3, '--double-next', 4, 5])
+//  // [1, 2, 3, 4, 4, 5]
+//  transform([1, 2, 3, '--discard-prev', 4, 5])
+//  // [1, 2, 4, 5]
+//  transform([1, 2, 3, '--double-prev', 4, 5])
+//  // [1, 2, 3, 3, 4, 5]
+//  transform([1, 2, 3, '--discard-next', 4, 5])
+//  // [1, 2, 3, 5]
+
+transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])
+transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5])
+transform([1, 2, 3, '--double-next'])
+transform([1, 2, 3, '--discard-next'])
+
+export default function transform(arr) {
+
+let newArr = arr.concat() ;
+
+if (newArr.includes('--double-prev')) {
+  if (newArr.indexOf('--double-prev') == 0) {
+    newArr.splice(newArr.indexOf('--double-prev'), 1)
+  } else {
+  newArr.splice(newArr.indexOf('--double-prev'), 0, newArr.indexOf('--double-prev'))
+  newArr.splice(newArr.indexOf('--double-prev'), 1)
+  }
+}
+
+if (newArr.includes('--discard-prev')) {
+  if (newArr.indexOf('--discard-prev') == 0) {
+    newArr.splice(newArr.indexOf('--discard-prev'), 1)
+  } else {
+    newArr.splice(newArr.indexOf('--discard-prev')-1, 2)
+  }
+}
+
+if (newArr.includes('--double-next')) {
+  if (newArr.indexOf('--double-next') == newArr.length - 1) {
+    newArr.splice(newArr.indexOf('--double-next'), 1)
+  } else {
+    newArr.splice(newArr.indexOf('--double-next'), 0, newArr.indexOf('--double-next')+1)
+    newArr.splice(newArr.indexOf('--double-next'), 1)
+  }
+}
+
+if (newArr.includes('--discard-next')) {
+  newArr.splice(newArr.indexOf('--discard-next'), 2)
+}
+
+
+if (!Array.isArray(arr)) {
+  return '\'arr\' parameter must be an instance of the Array!'
+  }
+
+// for (let i = 0; i < arr.length; i++) {
+  
+// if (arr[i] == '--double-next') {
+//   newArr.push(arr[i + 1])
+// } else if (arr[i] == '--double-prev') {
+//   newArr.push(arr[i - 1])
+// } else if (arr[i] == '--discard-prev') {
+//   newArr.splice(i-1, 1)
+// } else if (arr[i] == '--discard-next') {
+//   newArr.splice(i+1, 1)
+// } else {
+//   newArr.push(arr[i])
+// }
+// }
+
+
+return newArr;
+
 }
