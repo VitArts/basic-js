415faee779f755d4bbf1eea9334578561f2d26f2
VitArts <vitarts@ya.ru>
2021-09-13 15:48:29 +0300

    add: st-get-email-domain.js & st-encode-line.js

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ export default function encodeLine(str) {
+  let newStr = ''
+  let num = 1
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      num += 1
+    } else {
+      if (num > 1) {
+        newStr += num + str[i]
+      } else {
+        newStr += str[i]
+      }
+      num = 1
+    }
+  }
+  return newStr
 }
